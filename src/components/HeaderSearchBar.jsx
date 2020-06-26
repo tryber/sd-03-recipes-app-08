bbe57eac7c2247097aa2cffcd99c84d2a59c5f2c
Hfreitas <hfreitas86@gmail.com>
2020-06-26 11:53:40 -0300

    Criados elementos básicos e data-testid's

--- src/components/HeaderSearchBar.jsx
+++ src/components/HeaderSearchBar.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
 const HeaderSearchBar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Buscar Receita"
+        data-testid="search-input"
+      />
+      <label htmlFor="ingredient">
+        <input
+          type="radio"
+          name=""
+          id="ingredient"
+          data-testid="ingredient-search-radio"
+        />
+        Ingrediente
+      </label>
+      <label htmlFor="name">
+        <input type="radio" name="" id="name" data-testid="name-search-radio" />
+        Nome
+      </label>
+      <label htmlFor="first-letter">
+        <input
+          type="radio"
+          name=""
+          id="first-letter"
+          data-testid="first-letter-search-radio"
+        />
+        Primeira letra
+      </label>
+      <button type="button" data-testid="exec-search-btn">
+        Buscar
+      </button>
+    </div>
+  );
 };
 
 export default HeaderSearchBar;
