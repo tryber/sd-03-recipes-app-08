d33fbc7855e7a38bc5f7f2a4e6b82b9de9b47c85
Hfreitas <hfreitas86@gmail.com>
2020-06-26 13:25:40 -0300

    acrescentando css do modelo

--- src/components/HeaderSearchBar.jsx
+++ src/components/HeaderSearchBar.jsx
@@ -1,34 +1,43 @@
 import React from 'react';
+import '../styles/HeaderSearchBar.css';
 
 const HeaderSearchBar = () => (
-  <div>
+  <div className="search-container">
     <input
       type="text"
       id="search-input"
       placeholder="Buscar Receita"
       data-testid="search-input"
+      className="search-input"
     />
-    <label htmlFor="ingredient">
+    <label htmlFor="ingredient" className="ingredient-search-label">
       <input
         type="radio"
         id="ingredient"
         data-testid="ingredient-search-radio"
       />
       Ingrediente
+      <span className="checkmark" />
     </label>
-    <label htmlFor="name">
+    <label htmlFor="name" className="name-search-label">
       <input type="radio" id="name" data-testid="name-search-radio" />
       Nome
+      <span className="checkmark" />
     </label>
-    <label htmlFor="first-letter">
+    <label htmlFor="first-letter" className="first-letter-search-label">
       <input
         type="radio"
         id="first-letter"
         data-testid="first-letter-search-radio"
       />
       Primeira letra
+      <span className="checkmark" />
     </label>
-    <button type="button" data-testid="exec-search-btn">
+    <button
+      type="button"
+      data-testid="exec-search-btn"
+      className="search-button"
+    >
       Buscar
     </button>
   </div>
