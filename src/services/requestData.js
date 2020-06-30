f81184b02e826f4d778d9fac3b0e7e8f9d862f3e
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-06-30 19:29:53 -0300

    CC

--- src/services/requestData.js
+++ src/services/requestData.js
@@ -11,19 +11,29 @@ import {
   getFoodByFirstLetter,
 } from './meals&drinksAPI';
 
+const conditionalsearchFilters = (object) => {
+  if (object.filter === 'ingredient') return getDrinkByIngredient(object.value);
+  if (object.filter === 'name') return getDrinkByName(object.value);
+  if (object.filter === 'first-letter') return getDrinkByFirstLetter(object.value);
+  return getDrinkList();
+};
+
 export function requestDrinksData(categoriesFilter, searchFilters) {
   if (categoriesFilter !== 'All') return getDrinkByCategorie(categoriesFilter);
-  if (searchFilters.filter === 'ingredient') return getDrinkByIngredient(searchFilters.value);
-  if (searchFilters.filter === 'name') return getDrinkByName(searchFilters.value);
-  if (searchFilters.filter === 'first-letter') return getDrinkByFirstLetter(searchFilters.value);
-  return getDrinkList();
+  if (categoriesFilter === 'All' && searchFilters === { value: '', filter: '' }) return getDrinkList();
+  return conditionalsearchFilters(searchFilters);
 }
+
+const conditionalsearchFiltersFood = (object) => {
+  if (object.filter === 'ingredient') return getFoodByIngredient(object.value);
+  if (object.filter === 'name') return getFoodByName(object.value);
+  if (object.filter === 'first-letter') return getFoodByFirstLetter(object.value);
+  return getFoodList();
+};
 
 export function requestFoodsData(categoriesFilter, searchFilters) {
   if (categoriesFilter !== 'All') return getFoodByCategorie(categoriesFilter);
-  if (searchFilters.filter === 'ingredient') return getFoodByIngredient(searchFilters.value);
-  if (searchFilters.filter === 'name') return getFoodByName(searchFilters.value);
-  if (searchFilters.filter === 'first-letter') return getFoodByFirstLetter(searchFilters.value);
-  return getFoodList();
+  if (categoriesFilter === 'All' && searchFilters === { value: '', filter: '' }) return getFoodList();
+  return conditionalsearchFiltersFood(searchFilters);
 }
 
