ba6758436127a5f3f2a2b3c07a21c6939b832c8d
Hfreitas <hfreitas86@gmail.com>
2020-07-04 17:12:18 -0300

    componentizando pagina de exploração

---
+++ src/components/ExploreByPageLinks.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const ExploreByPageLinks = ({ location, callback, id }) => {
+  return (
+    <div>
+      <Link to={`${location}/ingredientes`}>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          className="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      {location === '/explorar/comidas' && (
+        <Link to="/explorar/comidas/area">
+          <button
+            type="button"
+            data-testid="explore-by-area"
+            className="explore-by-area"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+      )}
+      <Link to={callback(location, id)}>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          className="explore-surprise"
+        >
+          Me Surpreenda
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+ExploreByPageLinks.propTypes = {
+  callback: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+};
+
+export default ExploreByPageLinks;
--- src/pages/ExploreMealsOrDrinks.jsx
+++ src/pages/ExploreMealsOrDrinks.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import profileIcon from '../images/profileIcon.svg';
 import {
   createTitle,
 } from '../helpers/dataHandlers';
+import ExploreByPageLinks from '../components/ExploreByPageLinks';
 
 const pathToRandom = (location, id) => (location.pathname === '/explorar/comidas' ? `/comidas/${id}` : `/bebidas/${id}`);
 
@@ -15,35 +16,7 @@ function ExploreMealsOrDrinks() {
     <main className="explore-container">
       <Header iconProfile={profileIcon} title={`Explorar ${createTitle(location)}`} />
       <section className="explore-by-links-container">
-        <Link to={`${location.pathname}/ingredientes`}>
-          <button
-            type="button"
-            data-testid="explore-by-ingredient"
-            className="explore-by-ingredient"
-          >
-            Por Ingredientes
-          </button>
-        </Link>
-        {location.pathname === '/explorar/comidas' && (
-        <Link to="/explorar/comidas/area">
-          <button
-            type="button"
-            data-testid="explore-by-area"
-            className="explore-by-area"
-          >
-            Por Local de Origem
-          </button>
-        </Link>
-        )}
-        <Link to={pathToRandom(location.pathname, _)}>
-          <button
-            type="button"
-            data-testid="explore-surprise"
-            className="explore-surprise"
-          >
-            Me Surpreenda
-          </button>
-        </Link>
+      <ExploreByPageLinks callback={pathToRandom} id={} location={location.pathname} />
       </section>
       <Footer />
     </main>
