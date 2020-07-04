530bf43129e9c1dcb9acd3475c7a7b09b91e83e8
Hfreitas <hfreitas86@gmail.com>
2020-07-04 15:21:36 -0300

    ajustando imports

--- src/components/FetchHandlerContainer.jsx
+++ src/components/FetchHandlerContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../styles/DrinksPrincipalPage.css';
+import '../styles/RecipesPage.css';
 
 const FetchHandlerContainer = ({ loading, error }) => {
   if (loading) return <h1>Loading...</h1>;
