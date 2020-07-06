edaec8b27d8d6c38835eef4decd2303e11e654db
Hfreitas <hfreitas86@gmail.com>
2020-07-05 22:05:04 -0300

    card de ingredient finalizado

--- src/components/IngredientCard.jsx
+++ src/components/IngredientCard.jsx
@@ -1,31 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useBeverageOrMealsContext } from '../hooks';
 import '../styles/RecipeCard.css';
 
+const srcSwitch = (path, name) => (path === '/comidas' ? `https://www.themealdb.com/images/ingredients/${name}.png` : `https://www.thecocktaildb.com/images/ingredients/${name}-Small.png`);
+
 const IngredientCard = ({
-  thumbnail, name, index, path,
-}) => (
-  <Link to={`${path}`}>
-    <div data-testid={`${index}-recipe-card`} className="recipe-card">
-      <img
-        src={thumbnail}
-        alt={name}
-        data-testid={`${index}-card-img`}
-        className="recipe-image"
-      />
-      <h5 data-testid={`${index}-card-name`} className="recipe-title">
-        {name}
-      </h5>
-    </div>
-  </Link>
-);
+  name, index, path,
+}) => {
+  const setIngredient = useBeverageOrMealsContext(path);
+  return (
+    <Link
+      to={`${path}`}
+      onClick={() => setIngredient.setSearchFilters(() => ({
+        filter: 'ingredient',
+        value: { name },
+      }))}
+    >
+      <div data-testid={`${index}-ingredient-card`} className="recipe-card">
+        <img
+          src={srcSwitch(path, name)}
+          alt={name}
+          data-testid={`${index}-card-img`}
+          className="recipe-image"
+        />
+        <h5 data-testid={`${index}-card-name`} className="recipe-title">
+          {name}
+        </h5>
+      </div>
+    </Link>
+  );
+};
 
 IngredientCard.propTypes = {
   index: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
-  thumbnail: PropTypes.string.isRequired,
 };
 
 export default IngredientCard;
