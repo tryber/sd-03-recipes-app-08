493ec9b1538dbac4572bfcfdbbf1db8b0a69e57d
Hfreitas <hfreitas86@gmail.com>
2020-07-08 01:28:15 -0300

    resolvido bug que não resetava state

--- src/hooks/useBeverageOrMealsContext.js
+++ src/hooks/useBeverageOrMealsContext.js
@@ -1,8 +1,17 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { RecipeAppContext } from '../context';
 
 export default function useBeverageorMealsContext(location) {
   const { mealsData, beverageData } = useContext(RecipeAppContext);
+
+  useEffect(() => {
+    mealsData.setCategoriesFilter('All');
+    beverageData.setCategoriesFilter('All');
+    return () => {
+      mealsData.setCategoriesFilter('All');
+      beverageData.setCategoriesFilter('All');
+    };
+  }, [location]);
 
   if (location.pathname === '/comidas') return mealsData;
   return beverageData;
