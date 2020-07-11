63159acdec4d28408ba0eb2dd0f4c608e589a569
Gustavo <ggteodor07@gmai.com>
2020-07-11 12:53:43 -0300

    favorite-page functional 1.1

--- src/components/Clipboard.jsx
+++ src/components/Clipboard.jsx
@@ -3,25 +3,25 @@ import PropTypes from 'prop-types';
 import Icon from '../images/shareIcon.svg';
 import '../styles/RecipeDetails.css';
 
+const IconFunction = (id, choice, setIconUsed) => {
+  if (choice === 'meal') {
+    navigator.clipboard.writeText(`${window.location.origin}/comidas/${id}`);
+  }
+  if (choice === 'drink') {
+    navigator.clipboard.writeText(`${window.location.origin}/bebidas/${id}`);
+  }
+  setIconUsed(true);
+  setTimeout(() => setIconUsed(false), 2000);
+};
+
 const Clipboard = ({ id, choice, index }) => {
   const [iconUsed, setIconUsed] = useState(false);
-
-  const IconFunction = () => {
-    if (choice === 'meal') {
-      navigator.clipboard.writeText(`${window.location.origin}/comidas/${id}`);
-    }
-    if (choice === 'drink') {
-      navigator.clipboard.writeText(`${window.location.origin}/bebidas/${id}`);
-    }
-    setIconUsed(true);
-    setTimeout(() => setIconUsed(false), 2000);
-  };
 
   return (
     <div>
       <button
         type="button"
-        onClick={() => IconFunction()}
+        onClick={() => IconFunction(id, choice, setIconUsed)}
         className="share-icon"
       >
         <img
